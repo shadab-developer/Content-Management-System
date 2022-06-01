4138d5065013d1f6919010c06a17cc3bf260abb8
Shadab Ahmad <shadab.11900753@lpu.in>
2022-06-01 19:12:19 +0530

    new changes

--- include/functions.php
+++ include/functions.php
@@ -32,7 +32,7 @@ function postsFetch()
     $post_content = $row['post_content'];
     echo "<h2><a href='#'>$post_title</a></h2>";
     echo "<p class='lead'>by <a href='#'>$post_author</a></p>";
-    echo "<p><span class='glyphicon glyphicon-time'></span> Posted on $post_date at 10:00 PM</p> ";
+    echo "<p><span class='glyphicon glyphicon-time'></span> Posted on $post_date at 10:00 PM</p> <hr>";
 
     echo "<img class='img-responsive' src='$post_attachment' alt=''><hr>";
 
