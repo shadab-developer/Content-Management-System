548ae0bdb82c6eca69ebcea081bc1e3e96f38422
Shadab Ahmad <shadab.11900753@lpu.in>
2022-06-02 11:27:02 +0530

    new changes

--- include/functions.php
+++ include/functions.php
@@ -61,7 +61,7 @@ function search()
   if (isset($_POST['submit'])) {
     $search = $_POST['search'];
 
-    $query = "SELECT * FROM posts WHERE post_tags OR post_title OR post_content LIKE '%$search%'";
+    $query = "SELECT * FROM posts WHERE post_title OR post_content LIKE '%$search%'";
 
     $result = mysqli_query($conn, $query);
 
@@ -74,7 +74,21 @@ function search()
     if ($count == 0) {
       echo "<h2>NO RESULT</h2>";
     } else {
-      echo "<h2>we have post releated to your query</h2>";
+      while ($row = mysqli_fetch_assoc($result)) {
+        $post_title = $row['post_title'];
+        $post_author = $row['post_author'];
+        $post_date = $row['post_date'];
+        $post_attachment = $row['post_attachment'];
+        $post_content = $row['post_content'];
+        echo "<h2><a href='#'>$post_title</a></h2>";
+        echo "<p class='lead'>by <a href='#'>$post_author</a></p>";
+        echo "<p><span class='glyphicon glyphicon-time'></span> Posted on $post_date at 10:00 PM</p> <hr>";
+
+        echo "<img class='img-responsive' src='$post_attachment' alt=''><hr>";
+
+        echo "<p>$post_content</p>";
+        echo "<a class='btn btn-primary' href='#'>Read More <span class='glyphicon glyphicon-chevron-right'></span></a>";
+      }
     }
   }
 }
