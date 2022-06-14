a894f5aee796377765bf6a7754bcef1bc9bfd2c2
Shadab Ahmad <shadab.11900753@lpu.in>
2022-06-14 15:34:30 +0530

    new changes

--- admin/profile.php
+++ admin/profile.php
@@ -47,14 +47,16 @@
             <div class="container rounded bg-white mt-5 mb-5">
               <div class="row">
                 <div class="col-md-3 border-right">
-                  <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="../images/<?php echo $_SESSION['user_image'] ?>"> </span></div>
-                  <br><input type="file" name="user_image" class="form-control">
+                  <form action="" method="POST" enctype="multipart/form-data">
+                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="../images/<?php echo $_SESSION['user_image'] ?>"> </span></div>
+                    <br><input type="file" name="user_image" class="form-control">
                 </div>
                 <div class="col-md-5 border-right">
                   <div class="p-3 py-5">
                     <div class="d-flex justify-content-between align-items-center mb-3">
                       <h4 class="text-right">Profile Settings</h4>
                     </div>
+
                     <div class="row mt-2">
                       <div class="col-md-6"><label class="labels">First Name</label><input type="text" name="user_firstname" class="form-control" value="<?php echo $user_firstname ?>"></div>
                       <div class="col-md-6"><label class="labels">Last Name</label><input type="text" name="user_lastname" class="form-control" value="<?php echo $user_lastname ?>"></div>
@@ -64,7 +66,8 @@
                       <div class="col-md-12"><label class="labels">Email ID</label><input type="text" class="form-control" value="<?php echo $user_email ?>"></div>
                     </div>
                     <br>
-                    <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                    <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="submit" name="save_profile">Save Profile</button></div>
+                    </form>
                   </div>
                 </div>
 
@@ -73,12 +76,14 @@
                     <div class="d-flex justify-content-between align-items-center mb-3">
                       <h4 class="text-right">Security Settings</h4>
                     </div>
-                    <div class="col-md-12"><label class="labels">UserName</label><input type="text" class="form-control" value="<?php echo $user_username ?>"></div> <br>
-                    <div class="col-md-12"><label class="labels">Password</label><input type="password" class="form-control" value="<?php echo $user_password ?>"></div>
+                    <form action="" method="POST">
+                      <div class="col-md-12"><label class="labels">UserName</label><input type="text" class="form-control" value="<?php echo $user_username ?>"></div> <br>
+                      <div class="col-md-12"><label class="labels">Password</label><input type="password" class="form-control" value="<?php echo $user_password ?>"></div>
 
                   </div>
                   <div class="row mt-3">
-                    <div style="margin-top: 60px;" class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Security Settings</button></div>
+                    <div style="margin-top: 60px;" class="mt-5 text-center"><button class="btn btn-primary profile-button" type="submit" name="save_security">Security Settings</button></div>
+                    </form>
                   </div>
                 </div>
               </div>
